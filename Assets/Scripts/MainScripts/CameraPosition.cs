050f7659b36dcd24cec97c932f57bd1cc0553b92
Егор Кучук <KriptYashka@gmail.com>
2020-06-27 17:37:05 +0300

    Переделана камера
    Новая сцена - KriptYashka
    Обновленный префаб - tank_player
    Добавлено 2 скрипта:
    - CameraPosition
    - TankPosition

---
+++ Assets/Scripts/MainScripts/CameraPosition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosition : MonoBehaviour
+{
+    public Transform Rotate;
+    public Transform Target;
+    [Header("Скорость следования за танком")]
+    private float intens;
+    [SerializeField]
+    private float intensDefault = 0.15f;
+    [SerializeField]
+    private float intensMin = 0.02f;
+    [Header("Скорость следования за вращением башни")]
+    public float speed = 0.13f;
+    void Start()
+    {
+        transform.parent = null;
+        intens = intensDefault;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, Target.position, intens);
+
+        Vector3 relativePos = Rotate.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed);
+    }
+
+}
---
+++ Assets/Scripts/MainScripts/TankPosition.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankPosition : MonoBehaviour
+{
+    private Transform tank;
+    public Transform TarCamRot;
+    private Transform tarCam;
+    [Header("Скорость следования за вращением башни вокруг оси танка")]
+    public float speed = 0.13f;
+    [Space]
+    public float minAngle;
+    public float maxAngle;
+    public float speedCameraTarget;
+    [Space]
+    public float maxHight = -28;
+    public float minHight = -11;
+    [Space]
+    public LayerMask layerWall;
+    [Space]
+    public float correction;
+
+    private float angleHor;
+    private Quaternion originRotation;
+    [Header("Коррекция TarCam")]
+    public Vector2 startPos;
+    private Player _player;
+    void Start()
+    {
+        transform.parent = null; //Выход из зависимости
+        tarCam = transform.GetChild(0);
+        tank = TarCamRot.transform.parent;
+        transform.rotation = Quaternion.identity;
+        originRotation = transform.rotation;
+        tarCam.rotation = tank.rotation;
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = tank.position;
+
+        angleHor += Input.GetAxis("QE") * speedCameraTarget; //Присваиваем вращение переменной
+        angleHor = Mathf.Clamp(angleHor, minAngle, maxAngle); //Ограничиваем вращение
+        Quaternion rotationX = Quaternion.AngleAxis(angleHor, Vector3.right); //Опускаем подымаем камеру
+        Quaternion rotationY = Quaternion.AngleAxis(TarCamRot.eulerAngles.y, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, originRotation * rotationY * rotationX, speed); //Присваем вращение к камере
+
+        float normal = (angleHor + Mathf.Abs(minAngle)) / (Mathf.Abs(minAngle) + maxAngle); //Нормализованный вектор QE
+
+        Vector3 vec = new Vector3(startPos.x, startPos.y, ((normal) * maxHight) - Mathf.Abs(minHight) + (Mathf.Abs(minHight) * normal));
+        tarCam.localPosition = vec;
+
+        RaycastHit ray;
+        Vector3 start = TarCamRot.position - (transform.TransformPoint(vec) - TarCamRot.position) * correction;
+        Debug.DrawLine(start, transform.TransformPoint(vec), Color.blue);
+        if (Physics.Linecast(start, transform.TransformPoint(vec), out ray, layerWall))
+        {
+            tarCam.position = ray.point;
+        }
+    }
+}
